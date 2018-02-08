19090a82fcbba5cf7c839eb69c63bf9ce90f6387
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-08 15:00:26 -0600

    editted keyboardandmouse

--- keyboardandmouse/sketch.js
+++ keyboardandmouse/sketch.js
@@ -1,3 +1,34 @@
-// Demos of stuff 
+// Demos of stuff
 // Noah Besse
 // Feb 8, 2018 - replace with the date
+function setup()
+{
+  createCanvas(windowWidth,windowHeight);
+  background(255);
+}
+
+function draw()
+{
+  if (mouseIsPressed)
+  {
+    if (mouseButton === LEFT)
+    {
+      rect(mouseX,mouseY,200,400);
+    }
+    else if (mouseButton === RIGHT)
+    {
+      ellipse(mouseX,mouseY,100);
+    }
+  }
+  if (keyIsPressed)
+  {
+    if (key === 'w' || key === 'W')
+    {
+      background(255);
+    }
+    else if (key === 'B'|| key === 'b')
+    {
+      background(0);
+    }
+  }
+}
