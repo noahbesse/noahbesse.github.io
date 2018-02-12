6991be068ea1f2ea4b3659c91b21ce2599956b65
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-12 15:20:22 -0600

    editted

--- interactiveScene/sketch.js
+++ interactiveScene/sketch.js
@@ -1,12 +1,23 @@
 // Interactive Scene Project
 // Noah Besse
 // Feb, 9, 2018
-function setup()
-{
-  createCanvas(windowWidth,windowHeight);
-  let input = createInput();
-  input.position(20,65);
 
-  let button = createButton('submit');
-  button.position()
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+   let input = createInput('');
+   let stringTyped = input.value('');
+  input.position(windowWidth/2 - 90,windowHeight/2 - 30);
+
+  let button = createButton('Submit');
+  button.position(windowWidth/2 - 30,windowHeight/2);
+  button.mousePressed();
+}
+
+function draw(){
+ hello();
+}
+
+function hello(){
+  if (whatTheyTyped == "hello");
+  text('Hello!',windowWidth/2,windowHeight/2 + 200);
 }
