6d1cb338be6bfc440b7da1f858886cd5cb1a3e69
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-13 15:20:02 -0600

    more editting

--- interactiveScene/sketch.js
+++ interactiveScene/sketch.js
@@ -2,22 +2,27 @@
 // Noah Besse
 // Feb, 9, 2018
 
-function setup(){
-  createCanvas(windowWidth,windowHeight);
-   let input = createInput('');
-   let stringTyped = input.value('');
-  input.position(windowWidth/2 - 90,windowHeight/2 - 30);
-
-  let button = createButton('Submit');
-  button.position(windowWidth/2 - 30,windowHeight/2);
-  button.mousePressed();
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
-function draw(){
- hello();
+function draw() {
+
+
 }
 
-function hello(){
-  if (whatTheyTyped == "hello");
-  text('Hello!',windowWidth/2,windowHeight/2 + 200);
+function keyPressed() {
+  let keythatwaspressed = str(key);
+  let special = str(keyCode);
+  if (keyIsPressed) {
+    fill(random(255),random(255),random(255));
+    textSize(random(200));
+    text((keythatwaspressed || special), random(windowWidth),random(windowHeight));
+
+
+
+
+  }
+  return false;
 }
