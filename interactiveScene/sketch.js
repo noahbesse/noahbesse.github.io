b24f16e11980ce8706c857913f97c56920745c14
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-14 15:19:59 -0600

    editted more thingsssssssss

--- interactiveScene/sketch.js
+++ interactiveScene/sketch.js
@@ -6,19 +6,18 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 }
-
 function draw() {
 
 
 }
 
 function keyPressed() {
-  let keythatwaspressed = str(key);
-  let special = str(keyCode);
+  let keythatwaspressed = key;
+  // let special = str(keyCode);
   if (keyIsPressed) {
     fill(random(255),random(255),random(255));
     textSize(random(200));
-    text((keythatwaspressed), random(windowWidth),random(windowHeight));
+    text(keythatwaspressed, random(windowWidth),random(windowHeight));
   }
   return false;
 }
