aa346af72a75128ba0ba36d73919037b05e1171c
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-16 15:09:36 -0600

    added touch screen to interactive scene

--- interactiveScene/sketch.js
+++ interactiveScene/sketch.js
@@ -1,6 +1,7 @@
 // Interactive Scene Project
 // Noah Besse
 // Feb, 9, 2018
+let alphabet = ["A", "B", "C", "D","E","F","G","H","I","K","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -8,20 +9,35 @@ function setup() {
 }
 function draw() {
 
-
 }
 
 function keyPressed() {
-  let keythatwaspressed = key;
+  let keythatwaspressed = str(key);
   // let special = str(keyCode);
   if (keyIsPressed) {
-    fill(random(255),random(255),random(255));
-    textSize(random(200));
-    text(keythatwaspressed, random(windowWidth),random(windowHeight));
+    // let letter = match(keythatwaspressed,alphabet);
+    if (alphabet.includes(keythatwaspressed)){
+      textSize(random(100));
+      fill(random(255),random(255),random(255));
+      text(keythatwaspressed, random(windowWidth), random(windowHeight));
+      return false;
+    }
+    else{
+      return false;
+    }
   }
 }
+
 function mouseClicked() {
-  if (mouseIsPressed){
+  if (mouseButton === LEFT || CENTER || RIGHT){
+    fill(random(255),random(255),random(255));
+    textSize(random(50,100));
     text("CLICK", random(windowWidth),random(windowHeight));
+    return false;
   }
+}
+
+function  touchStarted(){
+  textSize(random(50,100));
+  fill(random(255),random(255),random(255));
 }
