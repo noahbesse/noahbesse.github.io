5b694dd64dca3a5e3bf7893efc2327f11f7884aa
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-28 14:57:49 -0600

    finished traffic light

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -5,17 +5,25 @@
 //GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 // function at processing.org/reference.
+
 let state;
+let redLightDuration, yellowLightDuration, greenLightDuration;
+let lastTimeLightChanged;
+
 function setup() {
   createCanvas(600, 600);
   state = 1;
+  redLightDuration = 3000;
+  yellowLightDuration = 500;
+  greenLightDuration = 3000;
+  lastTimeLightChanged = millis();
 }
-
 
 function draw() {
   background(255);
   drawOutlineOfLights();
-  changeLights();
+  checkIfLightSwitched();
+  displayCorrectLight();
 }
 
 function drawOutlineOfLights() {
@@ -23,36 +31,59 @@ function drawOutlineOfLights() {
   rectMode(CENTER);
   fill(0);
   rect(width/2, height/2, 75, 200, 10);
-
   //lights
   fill(255);
   ellipse(width/2, height/2 - 65, 50, 50); //top
   ellipse(width/2, height/2, 50, 50); //middle
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
-
-  if (millis() === 3000){
-    state = 3;
-  }
-  if (millis() === 6000){
-    state = 2;
-  }
-  if (millis() === 8000){
-    state = 1;
-  }
-
 }
 
-function changeLights(){
+function checkIfLightSwitched(){
   if (state === 1){
-    color("red");
-    ellipse(width/2, height/2 - 65, 50, 50);
+    if(millis() > lastTimeLightChanged + greenLightDuration){
+      state = 2;
+      lastTimeLightChanged = millis();
+    }
   }
   if (state === 2){
-    color("yellow");
-    ellipse(width/2, height/2, 50, 50);
+    if(millis() > lastTimeLightChanged + yellowLightDuration){
+      state = 3;
+      lastTimeLightChanged = millis();
+    }
   }
   if (state === 3){
-    color("green");
-    ellipse(width/2, height/2 + 65, 50, 50);
+    if(millis() > lastTimeLightChanged + redLightDuration){
+      state = 1;
+      lastTimeLightChanged = millis();
+    }
   }
+}
+
+function displayCorrectLight(){
+  if (state === 1){
+    drawGreenLight();
+  }
+  if (state === 2){
+    drawYellowLight();
+  }
+  if (state === 3){
+    drawRedLight();
+  }
+}
+
+
+function drawGreenLight(){
+  fill(0,255,0);
+  ellipse(width/2, height/2 + 65, 50, 50);
+}
+
+function drawRedLight(){
+  fill(255,0,0);
+  ellipse(width/2, height/2 - 65, 50, 50);
+}
+
+function drawYellowLight(){
+  fill(255, 255, 0);
+  ellipse(width/2, height/2, 50, 50);
+
 }
