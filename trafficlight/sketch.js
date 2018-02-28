953db80e631c434d008a1fa8346fa3ddd4c407ca
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-02-28 14:23:13 -0600

    editted trafficlight

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -5,11 +5,9 @@
 //GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 // function at processing.org/reference.
-let milliseconds;
 let state;
 function setup() {
   createCanvas(600, 600);
-  milliseconds = millis();
   state = 1;
 }
 
@@ -32,13 +30,13 @@ function drawOutlineOfLights() {
   ellipse(width/2, height/2, 50, 50); //middle
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
 
-  if (milliseconds === 3000){
+  if (millis() === 3000){
     state = 3;
   }
-  if (milliseconds === 6000){
+  if (millis() === 6000){
     state = 2;
   }
-  if (milliseconds === 8000){
+  if (millis() === 8000){
     state = 1;
   }
 
@@ -58,6 +56,3 @@ function changeLights(){
     ellipse(width/2, height/2 + 65, 50, 50);
   }
 }
-//function timer(){
-
-//}
