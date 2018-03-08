3fbf1642920e5de608cf38c8ba588633da6ada97
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-03-08 15:19:55 -0600

    editted more

--- stateVariableAssignment/sketch.js
+++ stateVariableAssignment/sketch.js
@@ -1,4 +1,5 @@
 let state;
+let sizeOfCar, topOfCar bottomOfCar, rightOfCar, leftOfCar;
 
 function setup(){
   createCanvas(800,800);
@@ -17,14 +18,14 @@ function startScreen(){
     rect(width/2,height/2,300,150);
     textSize(40);
     textAlign(CENTER);
-    text("Play",width/2,height/2);
+    text("Play",width/2,height/2 + 10);
     if (mouseX >= width/2 - 150 && mouseX <= width/2 + 150 && mouseY >= height/2 - 75 && mouseY <= height/2 +75){
       background(66, 244, 235);
       rectMode(CENTER);
       rect(width/2,height/2,350,175);
       textSize(50);
       textAlign(CENTER);
-      text("Play",width/2,height/2);
+      text("Play",width/2,height/2 + 15);
       if (mouseIsPressed){
         state = 1.5;
       }
@@ -43,7 +44,7 @@ function levelOne(){
     textSize(20);
     textAlign(CENTER);
     text("Click the Square to begin", width/2, height/2);
-    if (mouseIsPressed && (mouseX <= width - 645 && mouseX >= width - 695) && (mouseY <= height - 155 && mouseY >= height - 115)){
+    if (mouseIsPressed && (mouseX <= width - 645 && mouseX >= width - 695) && mouseY >= height - 155 && mouseY <= height - 115){
       state = 2;
     }
   }
@@ -52,7 +53,10 @@ function levelOne(){
     background(66, 244, 235);
     fill(255);
     rect(width /2,height/2,600,600);
-
+    car();
+    if (leftOfCar <= 200){
+      state = 5;
+    }
   }
 }
 
@@ -61,9 +65,17 @@ function car(){
   let y = mouseY;
   let sizeOfCar = 50;
   let topOfCar =  mouseY - 25;
-  let bottomOfCar = mouseY + 25;
-  let rightOfCar = mouseX + 25;
-  let leftOfCar = mouseX -25;
+  bottomOfCar = mouseY + 25;
+  rightOfCar = mouseX + 25;
+  leftOfCar = mouseX -25;
   rect(x,y,sizeOfCar,sizeOfCar);
 
+}
+
+function gameOver(){
+  if (state === 5){
+    background(66, 244, 235);
+    textAlign(CENTER);
+    text("GAME OVER",400,400);
+  }
 }
