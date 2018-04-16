ceeafb72547d8ce51be7cfb109a4c9b0943c4d08
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-04-15 21:43:10 -0600

    editted

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -1,3 +1,6 @@
+//Minesweeper
+//Noah Besse
+//April 14,2018
 let currentAmountOfMines
 let maxAmountOfMines = 10;
 let rows = 10;
@@ -46,4 +49,23 @@ function createRandom2dArray(cols, rows) {
     }
   }
   return randomGrid;
+}
+function createEmpty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x=0; x<cols; x++) {
+    randomGrid.push([]);
+    for (let y=0; y<rows; y++) {
+      randomGrid[x].push(0);
+    }
+  }
+  return randomGrid;
+}
+function detectMines(){
+  for (let i = 0; i < rows; i++){
+    for (let j = 0; j < cols; j++){
+      if (grid[i][j] === 1){
+        if grid[i + 1][j] ||
+      }
+    }
+  }
 }
