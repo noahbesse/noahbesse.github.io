3276f05e8f7303193901a8c655658f42d20f230a
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-04-15 21:42:52 -0600

    edited minesweeper

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -1,89 +1,49 @@
-//Traffic Light Starter Code
-//Dan Schellenberg
-//Feb 23, 2018
-
-//GOAL: make a 'traffic light' simulator. For now, just have the light
-// changing according to time. You may want to investigate the millis()
-// function at processing.org/reference.
-
-let state;
-let redLightDuration, yellowLightDuration, greenLightDuration;
-let lastTimeLightChanged;
+let currentAmountOfMines
+let maxAmountOfMines = 10;
+let rows = 10;
+let cols = 10;
+let grid;
+let cellSize;
 
 function setup() {
+  currentAmountOfMines = 0;
   createCanvas(600, 600);
-  state = 1;
-  redLightDuration = 3000;
-  yellowLightDuration = 500;
-  greenLightDuration = 3000;
-  lastTimeLightChanged = millis();
+  cellSize = width / cols;
+  grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
   background(255);
-  drawOutlineOfLights();
-  checkIfLightSwitched();
-  displayCorrectLight();
+  displayGrid();
 }
 
-function drawOutlineOfLights() {
-  //box
-  rectMode(CENTER);
-  fill(0);
-  rect(width/2, height/2, 75, 200, 10);
-  //lights
-  fill(255);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
-  ellipse(width/2, height/2, 50, 50); //middle
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
-}
+function displayGrid() {
+  for (let x=0; x<cols; x++) {
+    for (let y=0; y<rows; y++) {
+      if (grid[x][y] === 0 && currentAmountOfMines < maxAmountOfMines) {
 
-function checkIfLightSwitched(){
-  if (state === 1){
-    if(millis() > lastTimeLightChanged + greenLightDuration){
-      state = 2;
-      lastTimeLightChanged = millis();
-    }
-  }
-  if (state === 2){
-    if(millis() > lastTimeLightChanged + yellowLightDuration){
-      state = 3;
-      lastTimeLightChanged = millis();
-    }
-  }
-  if (state === 3){
-    if(millis() > lastTimeLightChanged + redLightDuration){
-      state = 1;
-      lastTimeLightChanged = millis();
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x=0; x<cols; x++) {
+    randomGrid.push([]);
+    for (let y=0; y<rows; y++) {
+      if (random(100) < 25 ) {
+        randomGrid[x].push(0);
 
-function displayCorrectLight(){
-  if (state === 1){
-    drawGreenLight();
+      }
+      else {
+        randomGrid[x].push(1);
+      }
+    }
   }
-  if (state === 2){
-    drawYellowLight();
-  }
-  if (state === 3){
-    drawRedLight();
-  }
-}
-
-
-function drawGreenLight(){
-  fill(0,255,0);
-  ellipse(width/2, height/2 + 65, 50, 50);
-}
-
-function drawRedLight(){
-  fill(255,0,0);
-  ellipse(width/2, height/2 - 65, 50, 50);
-}
-
-function drawYellowLight(){
-  fill(255, 255, 0);
-  ellipse(width/2, height/2, 50, 50);
-
+  return randomGrid;
 }
