a8f17c50a27989743902f609356fdd10189360c6
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-04-19 22:47:49 -0600

    aaaaaaaaaaaaaaaa

--- mineSweeper/sketch.js
+++ mineSweeper/sketch.js
@@ -1,6 +1,7 @@
 //Minesweeper
 //Noah Besse
 //April 14,2018
+let state;
 let rows = 10;
 let cols = 10;
 let grid;
@@ -18,13 +19,39 @@ function setup() {
   //with a lot of decimal places
   currentRow = Math.floor(mouseY / cellSize);
   currentCol = Math.floor(mouseX / cellSize);
+  state = 1;
+
+}
+function startScreen(){
+  if (state === 1){
+    rect(width/2 - 200,height/2 - 100,400,200);
+    textSize(48);
+    text("Press Space", width/2 - 130,height/2+20);
+    if (keyIsPressed){
+      if (keyCode === 32){
+      state = 2;
+      }
+    }
+  }
 }
 function draw() {
-  background(255);
-  stroke(5);
-  rect(0,0,600,600);
-  displayEmptyGrid();
+  startScreen();
+  gameScreen();
+  gameOver();
+}
 
+function gameScreen(){
+  if (state === 2){
+    displayEmptyGrid();
+    if (mouseIsPressed){
+      if (grid[currentCol][currentRow] === 0){
+           state = 3;
+         }
+      if (grid[currentCol][currentRow] ===1){
+           displayNeighbors();
+         }
+    }
+  }
 }
 function displayEmptyGrid(){
   for (let x=0; x<cols; x++) {
@@ -37,7 +64,6 @@ function displayGrid() {
   for (let x=0; x<cols; x++) {
     for (let y=0; y<rows; y++) {
       if (grid[x][y] === 0) {
-
         fill(0);
       }
       else {
@@ -52,7 +78,7 @@ function createRandom2dArray(cols, rows) {
   for (let x=0; x<cols; x++) {
     randomGrid.push([]);
     for (let y=0; y<rows; y++) {
-      if (random(100) < 25 ) {
+      if (random(100) < 20 ) {
         randomGrid[x].push(0);
 
       }
@@ -73,37 +99,32 @@ function createEmpty2dArray(cols, rows) {
   }
   return randomGrid;
 }
-function discorverNeighbors(){
-  let next = createEmpty2dArray();
+function displayNeighbors(){
   for (let x=0; x<cols; x++) {
     for (let y=0; y<rows; y++) {
 
       let neighbors = 0;
       for (let i=-1; i<=1; i++) {
         for (let j=-1; j<=1; j++) {
-          // don't break on the edges
           if (x+i >= 0  && x+i < cols && y+j >= 0 && y+j < rows) {
             neighbors += grid[x+i][y+j];
           }
         }
       }
-      if (neighbors === 0){
-        return false;
+      neighbors -= grid[x][y];
+
+      if (grid[x][y] === 1){
+        
+      }
+      else{
+
       }
 
     }
   }
 }
-function mousePressed(){
-
-  if (grid[currentCol][currentRow] === 0){
-    displayGrid();
-    gameOver();
-  }
-  if (grid[currentCol][currentRow] === 1){
-    displayNeighbors();
-  }
-}
 function gameOver(){
-  text("GAME OVER",width/2,height/2);
+  if (state === 3){
+    text("reeeee",0,0);
+  }
 }
