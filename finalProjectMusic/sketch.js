a6fa1d354e906dd61875b49ecc62894638a32e0b
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-05-04 15:18:17 -0600

    tfgjk

--- finalProjectMusic/sketch.js
+++ finalProjectMusic/sketch.js
@@ -1,34 +1,24 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// Music Visualizer
+// Noah Besse
+// May the fourth be with you, 2018
+let state;
 
-// global variables
-let gear;
-
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
+function setup(){
+  state = "welcome";
+  createCanvas(windowWidth,windowHeight);
 }
 
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function draw(){
+  background(0);
+  stateScreens();
 }
 
-function draw() {
-  background(255);
-
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+function stateScreens(){
+  if (state === "welcome"){
+    fill(0, 255, 0);
+    textFont("Agency FB");
+    textSize(windowWidth/25);
+    text("Welcome to Noah's Music Visualizer",windowWidth/8,windowHeight/2-200);
+    rect(windowWidth/10,windowHeight/2,50,25);
+  }
 }
