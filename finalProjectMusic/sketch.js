4912b90b4955cd3df8ebbf705b160da2867cb643
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-05-07 15:19:42 -0600

    sadasdasdad

--- finalProjectMusic/sketch.js
+++ finalProjectMusic/sketch.js
@@ -2,9 +2,11 @@
 // Noah Besse
 // May the fourth be with you, 2018
 let state;
+let musicFile;
+let dropZone = rect(windowWidth/2,windowHeight/2,200,200);
 
 function setup(){
-  state = "welcome";
+  state = 1;
   createCanvas(windowWidth,windowHeight);
 }
 
@@ -14,11 +16,20 @@ function draw(){
 }
 
 function stateScreens(){
-  if (state === "welcome"){
+  if (state === 1 /*Welcome Screen*/){
     fill(0, 255, 0);
     textFont("Agency FB");
     textSize(windowWidth/25);
     text("Welcome to Noah's Music Visualizer",windowWidth/8,windowHeight/2-200);
     rect(windowWidth/10,windowHeight/2,50,25);
+  }
+  if (state === 2/*Upload Screen*/){
+    dropZone.drop(gotFile);
+  }
+}
+
+function gotFile(file){
+  if (file.type === "wav" || file.type === "mp3"){
+    musicFile = loadSound(file);
   }
 }
